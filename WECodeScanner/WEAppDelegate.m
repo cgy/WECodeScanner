dda8717034ec35236204b5868df3a803c22c9ca7
freshlhy <freshlhy@gmail.com>
2014-03-06 10:03:38 -0800

    populate data success

--- WECodeScanner/WEAppDelegate.m
+++ WECodeScanner/WEAppDelegate.m
@@ -19,53 +19,54 @@
     [MagicalRecord setupCoreDataStackWithStoreNamed:@"DeviceModel"];
 
     if (![[NSUserDefaults standardUserDefaults] objectForKey:@"MR_HasPrefilledDevices"]) {
-//        NSString* contents = [NSString stringWithContentsOfFile:@"device.csv"
-//                                                       encoding:NSUTF8StringEncoding
-//                                                          error:nil];
-//        NSArray* lines = [contents componentsSeparatedByString:@"\n"];
-//        for (NSString* line in lines)
-//        {
-//            NSArray* fields = [line componentsSeparatedByString:@","];
-//            Device *d1 = [Device createEntity];
-//            d1.code  = fields[0];
-//            NSLog(@"%@", d1.code);
-//            d1.deviceDetails = [DeviceDetails createEntity];
-//            d1.deviceDetails.name = fields[1];
-//            d1.deviceDetails.classification = fields[2];
-//            d1.deviceDetails.type = fields[3];
-//            d1.deviceDetails.manufacturer = fields[4];
-//            d1.deviceDetails.brand = fields[5];
-//            d1.deviceDetails.series = fields[6];
-//            d1.deviceDetails.model = fields[7];
-//            d1.deviceDetails.since = fields[8];
-//            d1.deviceDetails.expire = fields[9];
-//            d1.deviceDetails.consumingDepartment = fields[10];
-//            d1.deviceDetails.user = fields[11];
-//            d1.deviceDetails.maintainingDepartment = fields[12];
-//            d1.deviceDetails.maintainer = fields[13];
-//            d1.deviceDetails.location = fields[14];
-//            d1.deviceDetails.ip = fields[15];
-//            d1.deviceDetails.network = fields[16];
-//        }
-        Device *d1 = [Device createEntity];
-        d1.code  = @"9771674449129";
-        d1.deviceDetails = [DeviceDetails createEntity];
-        d1.deviceDetails.name = @"Windows台式机";
-        d1.deviceDetails.classification = @"终端设备";
-        d1.deviceDetails.type = @"台式机";
-        d1.deviceDetails.manufacturer = @"联想";
-        d1.deviceDetails.brand = @"LENOVO";
-        d1.deviceDetails.series = @"联想";
-        d1.deviceDetails.model = @"联想";
-        d1.deviceDetails.since = @"2012-10-30";
-        d1.deviceDetails.expire = @"2015-10-07";
-        d1.deviceDetails.consumingDepartment = @"变电检修中心";
-        d1.deviceDetails.user = @"刘湖钰";
-        d1.deviceDetails.maintainingDepartment = @"变电检修中心";
-        d1.deviceDetails.maintainer = @"万意";
-        d1.deviceDetails.location = @"公司本部十一楼办公室";
-        d1.deviceDetails.ip = @"10.234.247.115";
-        d1.deviceDetails.network = @"内网";
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"device" ofType:@"csv"];
+        NSString* contents = [NSString stringWithContentsOfFile:path
+                                                       encoding:NSUTF8StringEncoding
+                                                          error:nil];
+        NSArray* lines = [contents componentsSeparatedByString:@"\n"];
+        for (NSString* line in lines)
+        {
+            NSArray* fields = [line componentsSeparatedByString:@","];
+            Device *d1 = [Device createEntity];
+            d1.code  = fields[0];
+            NSLog(@"%@", d1.code);
+            d1.deviceDetails = [DeviceDetails createEntity];
+            d1.deviceDetails.name = fields[1];
+            d1.deviceDetails.classification = fields[2];
+            d1.deviceDetails.type = fields[3];
+            d1.deviceDetails.manufacturer = fields[4];
+            d1.deviceDetails.brand = fields[5];
+            d1.deviceDetails.series = fields[6];
+            d1.deviceDetails.model = fields[7];
+            d1.deviceDetails.since = fields[8];
+            d1.deviceDetails.expire = fields[9];
+            d1.deviceDetails.consumingDepartment = fields[10];
+            d1.deviceDetails.user = fields[11];
+            d1.deviceDetails.maintainingDepartment = fields[12];
+            d1.deviceDetails.maintainer = fields[13];
+            d1.deviceDetails.location = fields[14];
+            d1.deviceDetails.ip = fields[15];
+            d1.deviceDetails.network = fields[16];
+        }
+//        Device *d1 = [Device createEntity];
+//        d1.code  = @"9771674449129";
+//        d1.deviceDetails = [DeviceDetails createEntity];
+//        d1.deviceDetails.name = @"Windows台式机";
+//        d1.deviceDetails.classification = @"终端设备";
+//        d1.deviceDetails.type = @"台式机";
+//        d1.deviceDetails.manufacturer = @"联想";
+//        d1.deviceDetails.brand = @"LENOVO";
+//        d1.deviceDetails.series = @"联想";
+//        d1.deviceDetails.model = @"联想";
+//        d1.deviceDetails.since = @"2012-10-30";
+//        d1.deviceDetails.expire = @"2015-10-07";
+//        d1.deviceDetails.consumingDepartment = @"变电检修中心";
+//        d1.deviceDetails.user = @"刘湖钰";
+//        d1.deviceDetails.maintainingDepartment = @"变电检修中心";
+//        d1.deviceDetails.maintainer = @"万意";
+//        d1.deviceDetails.location = @"公司本部十一楼办公室";
+//        d1.deviceDetails.ip = @"10.234.247.115";
+//        d1.deviceDetails.network = @"内网";
         
 //        d1.maintenance = [Maintenance createEntity];
 //        NSOrderedSet 
